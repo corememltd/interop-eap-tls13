ecab9d26707e1cefda8ad24e13df8039b989b15a
Alexander Clouter <aclouter@networkradius.com>
2021-09-23 15:54:31 +0100

    only need to sleep if it succeeded

--- services/dot1x-tunnel.sh
+++ services/dot1x-tunnel.sh
@@ -7,8 +7,7 @@ start)
 	LOCAL=${LOCAL:-$(ip route get 1.1.1.1 | sed -ne 's/.* src \([^ ]*\).*/\1/ p')}
 	REMOTE=${REMOTE:-$(ip route get 1.1.1.1 | sed -ne 's/.* via \([^ ]*\).*/\1/ p')}
 
-	ip l2tp del tunnel tunnel_id 1 || true
-	sleep 1 # racey :(
+	ip l2tp del tunnel tunnel_id 1 && sleep 1 || true	# racey
 
 	ip l2tp add tunnel local $LOCAL remote $REMOTE tunnel_id 1 peer_tunnel_id 1 encap udp udp_sport 1701 udp_dport 1701
 	ip l2tp add session name dot1x tunnel_id 1 session_id 0xffffffff peer_session_id 0xffffffff
@@ -18,7 +17,7 @@ start)
 	ip link set dot1x up
 	;;
 stop)
-	ip l2tp del tunnel tunnel_id 1 || true
+	ip l2tp del tunnel tunnel_id 1 && sleep 1 || true	# racey
 	;;
 *)	echo 'start or stop?' >&2
 	exit 1
